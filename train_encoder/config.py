937b0e538c4d87b2ab6425b66d6ce1158dbaa365
Håvard Stavnås Markhus <haavasma@stud.ntnu.no>
2023-01-23 17:06:25 +0100

    testing running scenario runner

---
+++ train_encoder/config.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Configuration:
+    # WIP configuration class
+    wip: int = 1
+    test: str = "test"
---
+++ train_encoder/main.py
@@ -0,0 +1,15 @@
+from config import Configuration
+
+
+def main():
+    print("Script for training the encoder")
+
+    return
+
+
+def train(config: Configuration):
+    print(config.wip)
+
+
+if __name__ == "__main__":
+    main()
