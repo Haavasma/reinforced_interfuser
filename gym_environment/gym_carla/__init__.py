892341a7601e0864ba530d1910cf8c3ca4280b4f
Håvard Stavnås Markhus <haavasma@stud.ntnu.no>
2023-03-07 00:03:57 +0100

    removed gym_envirnoment, opting to use separate environment package

--- gym_environment/gym_carla/__init__.py
+++
@@ -1,2 +0,0 @@
-from .envs.carla_env import CarlaEnvTransFuser
-from .envs.vanilla_rl_env import CarlaEnv
--- gym_environment/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="gym_carla",
-    version="0.0.1",
-    packages=[
-        package for package in find_packages() if package.startswith("gym_carla")
-    ],
-)
