85e0c714847dc83f280bea6ee2bc38fd6a070f3a
Håvard Stavnås Markhus <haavasma@stud.ntnu.no>
2023-04-11 18:01:43 +0200

    added split files per town for data generation

--- data_gen/scripts/generate_yamls.py
+++ data_gen/scripts/generate_yamls.py
@@ -4,10 +4,10 @@ import os
 d = {}
 d[
     "waypoint_disturb"
-] = 0.2  # in meters, a way of data augumentaion that randomly distrub the planned waypoints
+] = 0.5  # in meters, a way of data augumentaion that randomly distrub the planned waypoints
 d["waypoint_disturb_seed"] = 2020
 d["destory_hazard_actors"] = True
-d["save_skip_frames"] = 10  # skip 10 frames equals fps = 2
+d["save_skip_frames"] = 20  # skip 10 frames equals fps = 2
 d["rgb_only"] = False
 
 if not os.path.exists("yamls"):
