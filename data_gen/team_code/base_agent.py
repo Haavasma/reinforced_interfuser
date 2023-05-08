changed base_agent sensor specification to have correct lidar rotation

data_gen/team_code/base_agent.py
@@ -287,7 +287,7 @@ class BaseAgent(autonomous_agent.AutonomousAgent):
                 "z": 2.5,
                 "roll": 0.0,
                 "pitch": 0.0,
-                "yaw": -90.0,
+                "yaw": 90.0,
                 "id": "lidar",
             },
             {
@@ -575,7 +575,6 @@ class BaseAgent(autonomous_agent.AutonomousAgent):
         return cords
 
     def _translate_tl_state(self, state):
-
         if state == carla.TrafficLightState.Red:
             return 0
         elif state == carla.TrafficLightState.Yellow:
@@ -590,7 +589,6 @@ class BaseAgent(autonomous_agent.AutonomousAgent):
             return None
 
     def _get_affordances(self):
-
         # affordance tl
         affordances = {}
         affordances["traffic_light"] = None
@@ -608,7 +606,6 @@ class BaseAgent(autonomous_agent.AutonomousAgent):
         return affordances
 
     def _get_3d_bbs(self, max_distance=50):
-
         bounding_boxes = {
             "traffic_lights": [],
             "stop_signs": [],
@@ -671,7 +668,6 @@ class BaseAgent(autonomous_agent.AutonomousAgent):
                 )
 
         for vehicle in bb_3d["vehicles"]:
-
             trig_loc_world = self._create_bb_points(vehicle).T
             cords_x_y_z = self._world_to_sensor(
                 trig_loc_world, self._get_sensor_position(seg_cam), False
@@ -688,7 +684,6 @@ class BaseAgent(autonomous_agent.AutonomousAgent):
                     bounding_boxes["vehicles"].append(veh_bb)
 
         for pedestrian in bb_3d["pedestrians"]:
-
             trig_loc_world = self._create_bb_points(pedestrian).T
             cords_x_y_z = self._world_to_sensor(
                 trig_loc_world, self._get_sensor_position(seg_cam), False
@@ -716,7 +711,6 @@ class BaseAgent(autonomous_agent.AutonomousAgent):
         """
 
         for bb_type in bbs:
-
             _region = np.zeros(seg_img.shape)
 
             if bb_type == "traffic_light":
@@ -726,9 +720,7 @@ class BaseAgent(autonomous_agent.AutonomousAgent):
                     _region[box[0][1] : box[1][1], box[0][0] : box[1][0]] = 1
                 seg_img[(_region == 1)] = 23
             else:
-
                 for bb in bbs[bb_type]:
-
                     _region[bb[0][1] : bb[1][1], bb[0][0] : bb[1][0]] = 1
 
                 if bb_type == "stop_sign":
@@ -761,7 +753,6 @@ class BaseAgent(autonomous_agent.AutonomousAgent):
             )
 
             if 0 < distance_to_car <= max_distance:
-
                 if hasattr(_obstacle, "bounding_box"):
                     loc = _obstacle.bounding_box.location
                     _obstacle.get_transform().transform(loc)
@@ -874,7 +865,6 @@ class BaseAgent(autonomous_agent.AutonomousAgent):
         ).T
 
         if np.any(camera_bbox[:, 2] > 0):
-
             camera_bbox = np.array(camera_bbox)
             _positive_bb = camera_bbox[camera_bbox[:, 2] > 0]
 
@@ -906,7 +896,6 @@ class BaseAgent(autonomous_agent.AutonomousAgent):
             _region_size (int, optional): [description]. Defaults to 6.
         """
         for stop in stop_signs:
-
             _dist = self._get_distance(stop.get_transform().location)
 
             _region = np.abs(depth_img - _dist)
@@ -924,7 +913,6 @@ class BaseAgent(autonomous_agent.AutonomousAgent):
             ]
 
             if _x > 0:  # stop is in front of camera
-
                 bb = self._create_2d_bb_points(trigger, 4)
                 trig_loc_world = self._trig_to_world(bb, stop, trigger)
                 cords_x_y_z = self._world_to_sensor(
@@ -944,7 +932,6 @@ class BaseAgent(autonomous_agent.AutonomousAgent):
                 )
 
                 if np.any(camera_bbox[:, 2] > 0):
-
                     camera_bbox = np.array(camera_bbox)
 
                     polygon = [

data_gen/test_npy.py
@@ -1,8 +1,10 @@
 import numpy as np
 
 
-def main():
+from episode_manager.agent_handler.models.sensor import _to_bev
 
+
+def main():
     lidar_data = np.load(
         "../expert_data/routes_training_town01_w1_04_19_05_26_20/lidar/0038.npy"
     )

train_encoder/train.py
@@ -1339,7 +1339,6 @@ def train_one_epoch(
     model_ema=None,
     mixup_fn=None,
 ):
-
     second_order = hasattr(optimizer, "is_second_order") and optimizer.is_second_order
     batch_time_m = AverageMeter()
     data_time_m = AverageMeter()
@@ -1600,7 +1599,7 @@ def train_one_epoch(
                                     input["lidar"][0] / torch.max(input["lidar"][0]),
                                     0,
                                     1,
-                                )
+                                ).transpose(1, 2)
                             )
                         ],
                         "Train/Waypoints": [