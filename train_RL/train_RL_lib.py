c763278cce7bce555ceb5437be41dfc61301f877
haavasma <haavard.stavnas.markhus@gmail.com>
2023-05-20 16:25:03 +0200

    gpu assignment fixes

--- train_RL/train_RL_lib.py
+++ train_RL/train_RL_lib.py
@@ -377,7 +377,7 @@ def train(config: TrainingConfig) -> None:
 
     trainer_config = APPOConfig()  # if config["workers"] > 1 else PPOConfig()
 
-    gpu_fraction = (1 / (workers + 1)) - 0.01
+    gpu_fraction = (config["gpus"] / (workers + 2 if workers >1 else 1)) - 0.01
 
     algo_config = (
         trainer_config.rollouts(
@@ -491,7 +491,6 @@ def make_carla_env(
                 use_target_feature=True,
                 render_imitation=False,
                 postprocess=False,
-                gpu_device=i % gpus,
             )
             episode_config = interfuser_config()
 
