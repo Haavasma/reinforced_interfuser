51d42a8295d28c80988b1d9224b54971067f99cf
Havard Stavnas Markhus <haavasma@stud.ntnu.no>
2023-05-22 19:52:02 +0200

    updated requirements

--- train_RL/train.sh
+++ train_RL/train.sh
@@ -3,13 +3,13 @@
 #SBATCH --account=ie-idi
 #SBATCH --time=30:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=5
-#SBATCH --mem=50G
+#SBATCH --ntasks-per-node=8
+#SBATCH --mem=20G
 #SBATCH --job-name="baseline_agent"
 #SBATCH --output=test-baseline.out
 #SBATCH --mail-user=haavasma@stud.ntnu.no
 #SBATCH --mail-type=ALL
-#SBATCH --gres=gpu:V10032
+#SBATCH --gres=gpu:P100:2
 
 
 WORKDIR=${SLURM_SUBMIT_DIR}
@@ -30,14 +30,16 @@ echo "We are using $SLURM_CPUS_ON_NODE cores"
 echo "We are using $SLURM_CPUS_ON_NODE cores per node"
 echo "Total of $SLURM_NTASKS cores"
 
-module purge
-module load Anaconda3/2020.07
-eval "$(conda shell.bash hook)"
-conda activate rl_train
+#module purge
+#module load Anaconda3/2020.07
+#eval "$(conda shell.bash hook)"
+#conda activate rl_train
+
+source /cluster/home/haavasma/master/rl_train/bin/activate
 
 
-workers=3
-gpus=1
+workers=2
+gpus=2
 python train_RL_lib.py --workers $workers --gpus $gpus --no-traffic
 pkill -f CarlaUE4
 pkill -f CarlaUE4
